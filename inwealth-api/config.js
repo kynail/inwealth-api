c274efe34d4ef9144cb204e2e5b7399fb6aa0fa4
kynail <kylian.gence@epitech.eu>
2022-07-27 14:25:12 +0200

    add

--- inwealth-api/config.js
+++ inwealth-api/config.js
@@ -5,7 +5,7 @@ module.exports = {
   // USER: "inwealth",
   // PASSWORD: "diris2021",
   // DB: "inwealth",
-  HOST: '51.158.107.20',
+  HOST: '51.159.11.20',
   USER: 'inwealth',
   PASSWORD: 'Postgres1!',
   DB: 'inwealth',
