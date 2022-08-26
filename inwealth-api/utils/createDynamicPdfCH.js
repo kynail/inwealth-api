5b46d3d5011c91ed54b10b492ba40fd3bc5439f7
kynail <kylian.gence@epitech.eu>
2022-08-26 15:35:55 +0200

    add

--- inwealth-api/utils/createDynamicPdfCH.js
+++ inwealth-api/utils/createDynamicPdfCH.js
@@ -750,9 +750,9 @@ const createEnjeuxPdf = async ({ userID, data }) => {
         },
       },
     )
-    .rectangle(195, 200, 188, 38, { fill: '#FFFFFF' })
+    .rectangle(195, 191, 188, 38, { fill: '#FFFFFF' })
     .text(`${handleK(((data?.montantTrainDeVie || 0) - ((data?.revenuNetHorsImpot || 0) - (data?.chargeDontImpot || 0))))
-    } CHF`, 190, 200, {
+    } CHF`, 190, 191, {
       color: secondaryColor,
       size: 28,
       textBox: {
