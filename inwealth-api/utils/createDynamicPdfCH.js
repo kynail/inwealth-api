b52f6c40c369e74554103fa5bc60dc245aab0930
kynail <kylian.gence@epitech.eu>
2022-08-26 10:07:19 +0200

    add

--- inwealth-api/utils/createDynamicPdfCH.js
+++ inwealth-api/utils/createDynamicPdfCH.js
@@ -12,7 +12,7 @@ const HummusRecipe = require('hummus-recipe')
 const fs = require('fs')
 const path = require('path')
 const pdfDir = path.join(__dirname, `../static/pdf`)
-const templatePath = pdfDir + '/TempalteInw.pdf' // renommer le document TemplateInw Vs TempalteInw
+const templatePath = pdfDir + '/TemplateInwCh.pdf' // renommer le document TemplateInw Vs TempalteInw
 
 const { niveauFortuneEnum } = require('../enums')
 const { secteurActiviteEnum } = require('../enums')
@@ -386,6 +386,8 @@ const createProfilePdf = async ({ userID, data }) => {
 
   //Professionnelle
 
+  yLine = 160
+  lineSpacing = 23
   pdfDoc
     .editPage(5)
     .rectangle(265, 160, 675, 200, { fill: '#FFFFFF' })
@@ -490,39 +492,61 @@ const createProfilePdf = async ({ userID, data }) => {
     )
     .endPage()
 
-    
-  // Financiere
 
+
+  // Financiere
+  yLine = 185
+  lineSpacing = 23
   pdfDoc
-    .editPage(6)
-    .rectangle(770, 5, 190, 60, { fill: whiteColor })
-    .rectangle(747, yLine - 3, 200, 25, { fill: whiteColor })
+    .text(`•`, 265, yLine, {
+      color: secondaryColor,
+      size: 12,
+      textBox: {
+        textAlign: 'left center',
+        width: 20,
+        height: 24,
+      },
+    })
     .text(
-      niveauFortuneEnum[data?.niveauFortune] || 'Non renseignée',
-      747,
+      `Patrimoine hors endettement (comprenant la valorisation de votre participation) : ${niveauFortuneEnum[data?.niveauFortune] || 'Non renseignée'}`,
+      285,
       yLine,
       {
-        color: primaryColor,
+        color: '#2F2064',
         size: 12,
         textBox: {
           textAlign: 'left center',
-          width: 200,
-          height: 20,
+          width: 602,
+          height: 24,
         },
       },
     )
   yLine += lineSpacing
   pdfDoc
-    .rectangle(374, yLine - 3, 150, 25, { fill: whiteColor })
-    .text(`${handleK(data?.valorisationSteGroupe || 0)}€`, 374, yLine, {
-      color: primaryColor,
+    .text(`•`, 265, yLine, {
+      color: secondaryColor,
       size: 12,
       textBox: {
         textAlign: 'left center',
-        width: 150,
-        height: 20,
+        width: 20,
+        height: 24,
       },
     })
+    .text(
+      `Valorisation de votre participation : ${handleK(data?.valorisationSteGroupe || 0)}€`,
+      285,
+      yLine,
+      {
+        color: '#2F2064',
+        size: 12,
+        textBox: {
+          textAlign: 'left center',
+          width: 602,
+          height: 24,
+        },
+      },
+    )
+  yLine += lineSpacing
   pdfDoc
     .rectangle(421, 354, 150, 30, { fill: thirdColor })
     .text(`${handleK(data?.chargeDontImpot || 0)}€`, 421, 354, {
@@ -565,6 +589,32 @@ const createProfilePdf = async ({ userID, data }) => {
       },
     )
 
+  yLine += lineSpacing
+  pdfDoc
+    .text(`•`, 265, yLine, {
+      color: secondaryColor,
+      size: 12,
+      textBox: {
+        textAlign: 'left center',
+        width: 20,
+        height: 24,
+      },
+    })
+    .text(
+      `NB : Votre revenu net imposable : ${handleK(data?.revenuNetImposable || 0)}€`,
+      285,
+      yLine,
+      {
+        color: '#2F2064',
+        size: 12,
+        textBox: {
+          textAlign: 'left center',
+          width: 602,
+          height: 24,
+        },
+      },
+    )
+
   pdfDoc.endPage().endPDF((e) => {
     console.error(e)
   })
@@ -583,6 +633,7 @@ const createEnjeuxPdf = async ({ userID, data }) => {
   const pdfDoc = await new HummusRecipe(outputPath, outputPath)
   pdfDoc
     .editPage(3) // à compter de la page 7 qui est donc la 1ère page
+    // on affiche la valo en haut à droite
     .rectangle(700, 50, 150, 50, { fill: '#FFFFFF' })
     .text(`${handleK(data?.valorisationSteGroupe || 0)}€`, 600, 50, {
       color: secondaryColor,
@@ -594,80 +645,80 @@ const createEnjeuxPdf = async ({ userID, data }) => {
       },
     })
     .endPage()
-    .editPage(6) // Attention slide fiscalité liée à la cession qui dans le cas de la CH N/A
-    .rectangle(770, 3, 190, 58, { fill: whiteColor })
-    .rectangle(843, 295, 80, 25, { fill: '#FFFFFF' })
-    .text(
-      `${handleK((data?.valorisationSteGroupe * 12.8) / 100) || 0}€`,
-      843,
-      295,
-      {
-        color: secondaryColor,
-        size: 18,
-        textBox: {
-          textAlign: 'left center',
-          width: 200,
-          height: 25,
-        },
-      },
-    )
-    .rectangle(842, 345, 80, 25, { fill: '#FFFFFF' })
-    .text(
-      `${handleK((data?.valorisationSteGroupe * 17.2) / 100) || 0}€`,
-      842,
-      345,
-      {
-        color: secondaryColor,
-        size: 18,
-        textBox: {
-          textAlign: 'left center',
-          width: 200,
-          height: 25,
-        },
-      },
-    )
+    // .editPage(6) // Attention slide fiscalité liée à la cession qui dans le cas de la CH N/A
+    // .rectangle(770, 3, 190, 58, { fill: whiteColor })
+    // .rectangle(843, 295, 80, 25, { fill: '#FFFFFF' })
+    // .text(
+    //   `${handleK((data?.valorisationSteGroupe * 12.8) / 100) || 0}€`,
+    //   843,
+    //   295,
+    //   {
+    //     color: secondaryColor,
+    //     size: 18,
+    //     textBox: {
+    //       textAlign: 'left center',
+    //       width: 200,
+    //       height: 25,
+    //     },
+    //   },
+    // )
+    // .rectangle(842, 345, 80, 25, { fill: '#FFFFFF' })
+    // .text(
+    //   `${handleK((data?.valorisationSteGroupe * 17.2) / 100) || 0}€`,
+    //   842,
+    //   345,
+    //   {
+    //     color: secondaryColor,
+    //     size: 18,
+    //     textBox: {
+    //       textAlign: 'left center',
+    //       width: 200,
+    //       height: 25,
+    //     },
+    //   },
+    // )
 
-    .rectangle(842, 394, 80, 25, { fill: '#FFFFFF' })
-    .text(
-      `${handleK((data?.valorisationSteGroupe * 4) / 100) || 0}€`,
-      842,
-      403,
-      {
-        color: primaryColor,
-        size: 18,
-        textBox: {
-          textAlign: 'left center',
-          width: 200,
-          height: 25,
-        },
-      },
-    )
-    .rectangle(203, 367, 188, 38, { fill: '#FFFFFF' })
-    .text(
-      `${handleK((data?.valorisationSteGroupe * 34) / 100) || 0}€ ²`,
-      203,
-      367,
-      {
-        color: secondaryColor,
-        size: 32,
-        textBox: {
-          textAlign: 'center center',
-          width: 188,
-          height: 38,
-        },
-      },
-    )
-    .rectangle(600, 102, 150, 20, { fill: '#FFFFFF' })
-    .text(`${handleK(data?.valorisationSteGroupe || 0)}€`, 599, 105, {
-      color: secondaryColor,
-      size: 18,
-      textBox: {
-        textAlign: 'left center',
-        width: 155,
-        height: 20,
-      },
-    })
-    .endPage()
+    // .rectangle(842, 394, 80, 25, { fill: '#FFFFFF' })
+    // .text(
+    //   `${handleK((data?.valorisationSteGroupe * 4) / 100) || 0}€`,
+    //   842,
+    //   403,
+    //   {
+    //     color: primaryColor,
+    //     size: 18,
+    //     textBox: {
+    //       textAlign: 'left center',
+    //       width: 200,
+    //       height: 25,
+    //     },
+    //   },
+    // )
+    // .rectangle(203, 367, 188, 38, { fill: '#FFFFFF' })
+    // .text(
+    //   `${handleK((data?.valorisationSteGroupe * 34) / 100) || 0}€ ²`,
+    //   203,
+    //   367,
+    //   {
+    //     color: secondaryColor,
+    //     size: 32,
+    //     textBox: {
+    //       textAlign: 'center center',
+    //       width: 188,
+    //       height: 38,
+    //     },
+    //   },
+    // )
+    // .rectangle(600, 102, 150, 20, { fill: '#FFFFFF' })
+    // .text(`${handleK(data?.valorisationSteGroupe || 0)}€`, 599, 105, {
+    //   color: secondaryColor,
+    //   size: 18,
+    //   textBox: {
+    //     textAlign: 'left center',
+    //     width: 155,
+    //     height: 20,
+    //   },
+    // })
+    // .endPage()
     .editPage(7) //confort de vie annuel souhaité
     .rectangle(203, 367, 188, 38, { fill: '#FFFFFF' })
     .text(`${handleK(data?.montantTrainDeVie || 0)}€ nets`, 203, 367, {
@@ -698,7 +749,7 @@ const createObjectifPatPage = async ({ userID, piste }) => {
   const outputDir = path.join(__dirname, `../static/pdf/generated/${userID}`)
   const outputPath = `${outputDir}/objectifPat.pdf`
   const merger = new PDFMerger()
-  await merger.add(templatePath, '18, 19') // Vos pistes + Plan
+  await merger.add(templatePath, '14, 15') // Vos pistes + Plan
   await merger.save(outputPath)
   return outputPath
 }
@@ -706,7 +757,7 @@ const createPistePdf = async ({ userID, piste, data }) => {
   const outputDir = path.join(__dirname, `../static/pdf/generated/${userID}`)
   const outputPath = `${outputDir}/piste.pdf`
   const merger = new PDFMerger()
-  await merger.add(templatePath, '20 to 33') // les pistes de réflexion + + nom IP + quatrième de couverture 
+  await merger.add(templatePath, '16 to 39') // les pistes de réflexion + + nom IP + quatrième de couverture 
   await merger.save(outputPath)
   return outputPath
 }
