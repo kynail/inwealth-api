2f5364866b65ec297484ab7309df93bd422df46f
valfred <88735120+valfred@users.noreply.github.com>
2022-09-06 12:24:44 +0200

    add

--- inwealth-api/utils/createDynamicPdfCH2.js
+++ inwealth-api/utils/createDynamicPdfCH2.js
@@ -1,10 +1,4 @@
 const PDFMerger = require('pdf-merger-js')
-//FR
-/*
-
-Enjeu "Détenir l'immobilier d'entreprise"
-
-*/
 
 const {
   primaryColor,
@@ -18,8 +12,7 @@ const HummusRecipe = require('hummus-recipe')
 const fs = require('fs')
 const path = require('path')
 const pdfDir = path.join(__dirname, `../static/pdf`)
-const templatePath = pdfDir + '/TempalteInwFrImmo.pdf' 
-
+const templatePath = pdfDir + '/TempalteInwCh.pdf'
 
 const { niveauFortuneEnum } = require('../enums')
 const { secteurActiviteEnum } = require('../enums')
@@ -32,7 +25,7 @@ const { cantonEnum } = require('../enums')
 const { nationaliteEnum } = require('../enums')
 const imagePath = path.join(__dirname, '../static/images')
 
-const createDynamicPdf1 = async ({ userID, data, piste }) => {
+const createDynamicPdf = async ({ userID, data, piste }) => {
   try {
     const profilePdfPath = await createProfilePdf({ userID, data })
     const enjeuxPdfPath = await createEnjeuxPdf({ userID, data })
@@ -101,30 +94,30 @@ const createProfilePdf = async ({ userID, data }) => {
     )
   yLine += lineSpacing
   pdfDoc
-    .text(`•`, 265, yLine, {
-      color: secondaryColor,
+  .text(`•`, 265, yLine, {
+    color: secondaryColor,
+    size: 12,
+    textBox: {
+      textAlign: 'left center',
+      width: 20,
+      height: 24,
+    },
+  })
+  .text(
+    `Canton : ${cantonEnum[data?.canton] || ''}`,
+    285,
+    yLine,
+    {
+      color: '#2F2064',
       size: 12,
       textBox: {
         textAlign: 'left center',
-        width: 20,
+        width: 602,
         height: 24,
       },
-    })
-    .text(
-      `Canton : ${cantonEnum[data?.canton] || ''}`,
-      285,
-      yLine,
-      {
-        color: '#2F2064',
-        size: 12,
-        textBox: {
-          textAlign: 'left center',
-          width: 602,
-          height: 24,
-        },
-      },
-    )
-  yLine += lineSpacing
+    },
+  )
+yLine += lineSpacing
   pdfDoc
     .text(`•`, 265, yLine, {
       color: secondaryColor,
@@ -150,7 +143,7 @@ const createProfilePdf = async ({ userID, data }) => {
       },
     )
   yLine += lineSpacing
-
+  
   pdfDoc
     .text(`•`, 265, yLine, {
       color: secondaryColor,
@@ -418,12 +411,122 @@ const createProfilePdf = async ({ userID, data }) => {
     )
     .endPage()
 
+  //Professionnelle
+
+  yLine = 160
+  lineSpacing = 23
+  pdfDoc
+    .editPage(5)
+    .rectangle(265, 160, 675, 200, { fill: '#FFFFFF' })
+    .text(`•`, 265, yLine, {
+      color: secondaryColor,
+      size: 12,
+      textBox: {
+        textAlign: 'left center',
+        width: 20,
+        height: 24,
+      },
+    })
+    .text(
+      `Votre secteur d'activité : ${secteurActiviteEnum[data?.secteurActivite] || 'Non renseigné'}`,
+      285,
+      yLine,
+      {
+        color: '#2F2064',
+        size: 12,
+        textBox: {
+          textAlign: 'left center',
+          width: 602,
+          height: 24,
+        },
+      },
+    )
+  yLine += lineSpacing
+  pdfDoc
+    .text(`•`, 265, yLine, {
+      color: secondaryColor,
+      size: 12,
+      textBox: {
+        textAlign: 'left center',
+        width: 20,
+        height: 24,
+      },
+    })
+    .text(
+      `La forme juridique de la société dans laquelle vous détenez votre participation : ${typeSocieteEnum[data?.formeJuridique] || 'Non renseignée'}`,
+      285,
+      yLine,
+      {
+        color: '#2F2064',
+        size: 12,
+        textBox: {
+          textAlign: 'left center',
+          width: 602,
+          height: 24,
+        },
+      },
+    )
+  yLine += lineSpacing
+  pdfDoc
+    .text(`•`, 265, yLine, {
+      color: secondaryColor,
+      size: 12,
+      textBox: {
+        textAlign: 'left center',
+        width: 20,
+        height: 24,
+      },
+    })
+    .text(
+      `Vous détenez votre participation au travers d'une société holding  : ${data?.groupeOuiNon ? 'oui' : 'non'}`,
+      285,
+      yLine,
+      {
+        color: '#2F2064',
+        size: 12,
+        textBox: {
+          textAlign: 'left center',
+          width: 602,
+          height: 24,
+        },
+      },
+    )
+  yLine += lineSpacing
+  pdfDoc
+    .text(`•`, 265, yLine, {
+      color: secondaryColor,
+      size: 12,
+      textBox: {
+        textAlign: 'left center',
+        width: 20,
+        height: 24,
+      },
+    })
+    .text(
+      `Vous détenez l'immobilier d'exploitation : ${detentionImmoExploitationEnum[data?.immobilierExploitation] ||
+      'Non renseigné'}`,
+      285,
+      yLine,
+      {
+        color: '#2F2064',
+        size: 12,
+        textBox: {
+          textAlign: 'left center',
+          width: 602,
+          height: 24,
+        },
+      },
+    )
+    .endPage()
+
+
+
   // Financiere
   yLine = 185
   lineSpacing = 23
   pdfDoc
-    .editPage(5)
-    .rectangle(265, 160, 675, 100, { fill: '#FFFFFF' })
+  .editPage(6)
+  .rectangle(265, 160, 675, 100, { fill: '#FFFFFF' })
     .text(`•`, 265, yLine, {
       color: secondaryColor,
       size: 12,
@@ -554,11 +657,98 @@ const createEnjeuxPdf = async ({ userID, data }) => {
   const outputDir = path.join(__dirname, `../static/pdf/generated/${userID}`)
   const outputPath = `${outputDir}/enjeux.pdf`
   const merger = new PDFMerger()
-  await merger.add(templatePath, '6 to 12')
+  await merger.add(templatePath, '7 to 13')
   await merger.save(outputPath)
   const pdfDoc = await new HummusRecipe(outputPath, outputPath)
-  
   pdfDoc
+    .editPage(3) // à compter de la page 7 qui est donc la 1ère page
+    // on affiche la valo en haut à droite
+    .rectangle(700, 50, 200, 50, { fill: '#FFFFFF' })
+    .text(`${handleK(data?.valorisationSteGroupe || 0)} CHF`, 600, 50, {
+      color: secondaryColor,
+      size: 32,
+      textBox: {
+        textAlign: 'center center',
+        width: 350,
+        height: 50,
+      },
+    })
+    .endPage()
+    // .editPage(6) // Attention slide fiscalité liée à la cession qui dans le cas de la CH N/A
+    // .rectangle(770, 3, 190, 58, { fill: whiteColor })
+    // .rectangle(843, 295, 80, 25, { fill: '#FFFFFF' })
+    // .text(
+    //   `${handleK((data?.valorisationSteGroupe * 12.8) / 100) || 0} CHF`,
+    //   843,
+    //   295,
+    //   {
+    //     color: secondaryColor,
+    //     size: 18,
+    //     textBox: {
+    //       textAlign: 'left center',
+    //       width: 200,
+    //       height: 25,
+    //     },
+    //   },
+    // )
+    // .rectangle(842, 345, 80, 25, { fill: '#FFFFFF' })
+    // .text(
+    //   `${handleK((data?.valorisationSteGroupe * 17.2) / 100) || 0} CHF`,
+    //   842,
+    //   345,
+    //   {
+    //     color: secondaryColor,
+    //     size: 18,
+    //     textBox: {
+    //       textAlign: 'left center',
+    //       width: 200,
+    //       height: 25,
+    //     },
+    //   },
+    // )
+
+    // .rectangle(842, 394, 80, 25, { fill: '#FFFFFF' })
+    // .text(
+    //   `${handleK((data?.valorisationSteGroupe * 4) / 100) || 0} CHF`,
+    //   842,
+    //   403,
+    //   {
+    //     color: primaryColor,
+    //     size: 18,
+    //     textBox: {
+    //       textAlign: 'left center',
+    //       width: 200,
+    //       height: 25,
+    //     },
+    //   },
+    // )
+    // .rectangle(203, 367, 188, 38, { fill: '#FFFFFF' })
+    // .text(
+    //   `${handleK((data?.valorisationSteGroupe * 34) / 100) || 0} CHF ²`,
+    //   203,
+    //   367,
+    //   {
+    //     color: secondaryColor,
+    //     size: 32,
+    //     textBox: {
+    //       textAlign: 'center center',
+    //       width: 188,
+    //       height: 38,
+    //     },
+    //   },
+    // )
+    // .rectangle(600, 102, 150, 20, { fill: '#FFFFFF' })
+    // .text(`${handleK(data?.valorisationSteGroupe || 0)} CHF`, 599, 105, {
+    //   color: secondaryColor,
+    //   size: 18,
+    //   textBox: {
+    //     textAlign: 'left center',
+    //     width: 155,
+    //     height: 20,
+    //   },
+    // })
+    // .endPage()
+    pdfDoc
     .editPage(7) //confort de vie annuel souhaité
 
     .rectangle(421, 354, 150, 30, { fill: thirdColor })
@@ -571,11 +761,11 @@ const createEnjeuxPdf = async ({ userID, data }) => {
         height: 30,
       },
     })
-  pdfDoc
+    pdfDoc
     .rectangle(734, 354, 150, 30, { fill: thirdColor })
     .text(
       `${handleK(
-        (data?.revenuNetHorsImpot || 0) - (data?.chargeDontImpot || 0))} CHF`, 734, 354,
+        (data?.revenuNetHorsImpot || 0) - (data?.chargeDontImpot || 0),)} CHF`, 734, 354,
       {
         color: '#000000',
         size: 12,
@@ -589,7 +779,7 @@ const createEnjeuxPdf = async ({ userID, data }) => {
     )
     .rectangle(194, 187, 188, 38, { fill: '#FFFFFF' })
     .text(`${handleK(((data?.montantTrainDeVie || 0) - ((data?.revenuNetHorsImpot || 0) - (data?.chargeDontImpot || 0))))
-      } CHF`, 190, 187, {
+    } CHF`, 190, 187, {
       color: secondaryColor,
       size: 28,
       textBox: {
@@ -617,7 +807,7 @@ const createObjectifPatPage = async ({ userID, piste }) => {
   const outputDir = path.join(__dirname, `../static/pdf/generated/${userID}`)
   const outputPath = `${outputDir}/objectifPat.pdf`
   const merger = new PDFMerger()
-  await merger.add(templatePath, '13, 14') // Vos pistes + Plan
+  await merger.add(templatePath, '14, 15') // Vos pistes + Plan
   await merger.save(outputPath)
   return outputPath
 }
@@ -625,9 +815,9 @@ const createPistePdf = async ({ userID, piste, data }) => {
   const outputDir = path.join(__dirname, `../static/pdf/generated/${userID}`)
   const outputPath = `${outputDir}/piste.pdf`
   const merger = new PDFMerger()
-  await merger.add(templatePath, '15 to 19') // les pistes de réflexion + + nom IP + quatrième de couverture 
+  await merger.add(templatePath, '16 to 39') // les pistes de réflexion + + nom IP + quatrième de couverture 
   await merger.save(outputPath)
   return outputPath
 }
 
-module.exports = createDynamicPdf1
+module.exports = createDynamicPdf
