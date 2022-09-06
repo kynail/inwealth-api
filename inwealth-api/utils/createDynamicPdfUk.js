b786ae72776b4e328e6e0d01db05e5476b89b0aa
kynail <57899519+kynail@users.noreply.github.com>
2022-09-06 09:23:33 +0200

    zz

--- inwealth-api/utils/createDynamicPdfUk.js
+++ inwealth-api/utils/createDynamicPdfUk.js
@@ -28,14 +28,14 @@ const imagePath = path.join(__dirname, '../static/images')
 const createDynamicPdf1 = async ({ userID, data, piste }) => {
   try {
     const profilePdfPath = await createProfilePdf({ userID, data })
-    const enjeuxPdfPath = await createEnjeuxPdf({ userID, data })
+    // const enjeuxPdfPath = await createEnjeuxPdf({ userID, data })
     const objectifPdfPath = await createObjectifPatPage({ userID, piste })
     const pistePdfPath = await createPistePdf({ userID, piste, data })
     const outputDir = path.join(__dirname, `../static/pdf/generated/${userID}`)
     const outputPath = `${outputDir}/Reflexion patrimoniale.pdf`
     const merger = new PDFMerger()
     await merger.add(profilePdfPath)
-    await merger.add(enjeuxPdfPath)
+    // await merger.add(enjeuxPdfPath)
     await merger.add(objectifPdfPath)
     await merger.add(pistePdfPath)
     await merger.save(outputPath)
@@ -354,7 +354,7 @@ const createProfilePdf = async ({ userID, data }) => {
   return outputPath
 }
 
-// const handleK = (value) => (value >= 1_000 ? `${value / 1_000} K` : `${value}`)
+const handleK = (value) => (value >= 1_000 ? `${value / 1_000} K` : `${value}`)
 
 // const createEnjeuxPdf = async ({ userID, data }) => {
 //   const outputDir = path.join(__dirname, `../static/pdf/generated/${userID}`)
